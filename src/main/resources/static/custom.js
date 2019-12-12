f5527cce4dd2627d897af9be73be9094982fd565
Ryan Strobolakos <ryanstrobolakos@Ryans-MacBook-Air.local>
2019-12-12 10:26:39 -0500

    updating custom js to allow scrolling without zoom and default coordinates

--- src/main/resources/static/custom.js
+++ src/main/resources/static/custom.js
@@ -1,7 +1,14 @@
 var map;
+var coords = {lat: 39.9612, lng: -82.9988};
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 39.9612, lng: -82.9988},
-        zoom: 10
+        center: coords,
+        zoom: 10,
+        scrollwheel: false
+    });
+    
+    var marker = new google.maps.Marker({
+        position: coords,
+        map: map
     });
 }
