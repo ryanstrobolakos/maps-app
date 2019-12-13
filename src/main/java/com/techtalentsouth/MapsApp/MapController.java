400c284ccf423fd18ed074385bb749f1f19552a8
Ryan Strobolakos <ryanstrobolakos@Ryans-MacBook-Air.local>
2019-12-13 07:56:28 -0500

    updating get method and adding post method to handle form input for index page and return location

--- src/main/java/com/techtalentsouth/MapsApp/MapController.java
+++ src/main/java/com/techtalentsouth/MapsApp/MapController.java
@@ -2,7 +2,9 @@ package com.techtalentsouth.MapsApp;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MapController {
@@ -10,14 +12,16 @@ public class MapController {
 	private MapService mapService;
 	
 	@GetMapping("/home")
-    public String getDefaultMap() {
-		Location location = new Location();
-		location.setCity("Dallas");
-		location.setState("Texas");
-		mapService.addCoordinates(location);
-		System.out.println(location);
-		
-        return "index.html";
-    }
+	public String getDefaultMap(Model model) {
+	    model.addAttribute(new Location());
+	    return "index.html";
+	}
+	
+	@PostMapping("/home")
+	public String getMapForLocation(Location location, Model model) {
+	    mapService.addCoordinates(location);
+	    model.addAttribute(location);
+	    return "index.html";
+	}
 
 }
